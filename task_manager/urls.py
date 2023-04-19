e45847d4821ead1ead00720e9c0d38fd809c8bab
Arina Zubova <arina777pototskaya@gmail.com>
2023-04-19 13:13:14 +0400

    fix flash message for log out

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from task_manager import views
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', views.MyLoginView.as_view(redirect_authenticated_user=True),
+         name='login'),
+    path('logout/', views.MyLogoutView.as_view(), name='logout'),
     path('users/', include('users.urls')),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,20 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 
 class IndexView(TemplateView):
     template_name = "home_page.html"
+
+
+class MyLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'login.html'
+    success_message = 'You are logged in'
+
+
+class MyLogoutView(SuccessMessageMixin, LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        messages.add_message(request, messages.INFO, 'You are logged out')
+        return response
