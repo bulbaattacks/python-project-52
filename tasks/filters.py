b546f7bb79ed229b33190e8a91ae0fd064aa7920
Arina Zubova <arina777pototskaya@gmail.com>
2023-05-04 19:54:00 +0400

    add my_task only

--- tasks/filters.py
+++ tasks/filters.py
@@ -2,11 +2,15 @@ from .models import Task
 from statuses.models import Status
 from labels.models import Label
 from users.models import User
-from django_filters import FilterSet, ModelChoiceFilter
+from django_filters import FilterSet, ModelChoiceFilter, BooleanFilter
 from django import forms
 
 
 class TaskFilter(FilterSet):
+
+    def my_task_filter(self, queryset):
+        author = self.request.user
+        return queryset.filter(creator=author)
 
     status = ModelChoiceFilter(queryset=Status.objects.all(),
                                label='Status',
@@ -23,6 +27,12 @@ class TaskFilter(FilterSet):
                                  widget=forms.Select(
                                      attrs={'class': 'custom-select d-block'}))
 
+    mine = BooleanFilter(label='My tasks only',
+                         widget=forms.widgets.CheckboxInput(
+                             attrs={'name': 'mine'}),
+                         method='my_task_filter',
+                         )
+
     class Meta:
         model = Task
-        fields = ['status', 'label', 'assignee']
+        fields = ['status', 'label', 'assignee', 'mine']
